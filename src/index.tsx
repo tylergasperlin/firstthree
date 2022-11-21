1a9f7713f04d8bc74d0bb5251db534c93703513f
tylergasperlin <gasperlint@gmail.com>
2022-11-20 19:41:37 -0800

    Build out demo

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,49 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as THREE from 'three';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// React
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+// ThreeJS
+function animation(
+    time: any,
+    mesh: THREE.Mesh<THREE.BoxGeometry, THREE.MeshNormalMaterial>,
+    renderer: THREE.WebGLRenderer,
+    scene: THREE.Scene,
+    camera: THREE.PerspectiveCamera
+) {
+    mesh.rotation.x = time / 2000;
+    mesh.rotation.y = time / 1000;
+
+    renderer.render(scene, camera);
+}
+// init
+console.log('renders');
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+camera.position.z = 1;
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+const material = new THREE.MeshNormalMaterial();
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.setAnimationLoop((a) => {
+    animation(a, mesh, renderer, scene, camera);
+});
+document.body.appendChild(renderer.domElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
